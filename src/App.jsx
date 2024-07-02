2e07e1ead8a68c7de5cbbdb44b2256058e3c6b30
tecmarcelo78 <tecmarcelo78@outlook.com>
2024-07-02 17:42:22 -0300

    initial commit - test react

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>
+          Teste de REact
+        </h1>
+        <h2>Teste de rescue 2</h2>
+        <h3>Teste de rescue 3</h3>
+        <h4></h4>
+        <li>
+          <a href="http://www.uol.com.br">Site Uol</a>
+          <a href="http://terra.com.br">Site terra</a> 
+          <a href="http://google.com">Motor busca Google</a>
+          </li>
         <a
           className="App-link"
           href="https://reactjs.org"
